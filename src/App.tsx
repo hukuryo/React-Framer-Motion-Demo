ee247a48470ee8372a48ac75e6add4e024d18aaa
hukuryo <ryohei.1998.05.09@gmail.com>
2024-04-13 21:41:37 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { motion } from 'framer-motion'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className='w-[170px] h-[170px] rounded-full bg-red-500'></div>
+    </div>
+  )
+}
+
+export default App
