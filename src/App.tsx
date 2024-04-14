0a24ba46de7f67704677e013fcec59dbf6780914
hukuryo <ryohei.1998.05.09@gmail.com>
2024-04-14 17:27:14 +0900

    他の基本的な構文を学習

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { motion } from 'framer-motion'
 import './App.css'
+import BasicSintax from './components/BasicSintax'
 
 const App = () => {
   return (
     <div className='flex justify-center items-center'>
-      <motion.div
-        initial={{ x: -100 }}
-        animate={{ x: 200 }}
-        transition={{ duration: 2, delay: 2, ease: 'ease-in' }}
-        className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-      </motion.div>
+      <BasicSintax />
     </div>
   )
 }
---
+++ src/components/BasicSintax.tsx
@@ -0,0 +1,51 @@
+import { motion } from 'framer-motion'
+import '../App.css'
+
+const BasicSintax = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      {/* intial */}
+      {/* <motion.div
+        initial={{ x: -100 }}
+        className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+      </motion.div> */}
+
+      {/* animate */}
+      {/* <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 200 }}
+        className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+      </motion.div> */}
+
+      {/* transition-duration */}
+      {/* <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 200 }}
+        transition={{ duration: 10 }}
+        className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+      </motion.div> */}
+
+      {/* transiton-delay */}
+      {/* <motion.div
+        initial={{ x: -100 }}
+        animate={{ x: 1000 }}
+        transition={{ duration: 2, delay: 10, ease: 'ease-in' }}
+        className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+      </motion.div> */}
+
+        {/* whileTap */}
+        <motion.div
+            whileTap={{ scale: 2 }}
+            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+        </motion.div>
+
+        {/* whileHover */}
+        <motion.div
+            whileHover={{ opacity: 0 }}
+            className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+        </motion.div>
+    </div>
+  )
+}
+
+export default BasicSintax
