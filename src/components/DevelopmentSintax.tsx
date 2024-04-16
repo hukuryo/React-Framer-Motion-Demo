97fdb558ebc4b39c7aacc99ec03b7f389582354c
hukuryo <ryohei.1998.05.09@gmail.com>
2024-04-16 22:22:08 +0900

    transition-times practice

--- src/components/DevelopmentSintax.tsx
+++ src/components/DevelopmentSintax.tsx
@@ -2,6 +2,17 @@ import { motion } from 'framer-motion'
 import '../App.css'
 
 const DevelopmentSintax = () => {
+    const variants = {
+        animate: {
+          x: [-100, 200, -500],
+          transition: {
+            duration: 2,
+            times: [0, 0.75, 1], 
+            repeat: 2, 
+            repeatDelay: 2 
+          }
+        }
+      };
   return (
     <div className='flex justify-center items-center'>
         {/* <motion.div
@@ -23,7 +34,7 @@ const DevelopmentSintax = () => {
                     repeatDelay: 2 }}
             className='w-[170px] h-[170px] rounded-full bg-sky-500'>
         </motion.div> */}
-        <motion.div
+        {/* <motion.div
             animate={{ x: [-100, 200, -500] }}
             transition={{
                     duration: 2,
@@ -32,9 +43,22 @@ const DevelopmentSintax = () => {
                     repeatDelay: 2,
                     repeatType: 'reverse' }}
             className='w-[170px] h-[170px] rounded-full bg-sky-500'>
-        </motion.div>
+        </motion.div> */}
+        {/* <motion.button
+            initial={{ opacity: 0.6 }}
+            whileHover={{
+                scale: 1.2,
+                transition: { duration: 1 },
+            }}
+            whileTap={{ scale: 0.9 }}
+            whileInView={{ opacity: 1 }}
+            /> */}
+             <motion.div
+                variants={variants}
+                animate="animate"
+                className='w-[170px] h-[170px] rounded-full bg-sky-500'>
+            </motion.div>
     </div>
-
   )
 }
 
